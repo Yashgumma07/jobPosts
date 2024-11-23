ddf27ac653f513c8539ef623e9c53cd07a5f1f57
Yaswanth <yaswanthgummadi673@gmail.com>
2024-11-23 14:09:17 +0530

    "engines":{     "node": "18.x"   }, added

--- src/config/db.js
+++ src/config/db.js
@@ -6,8 +6,6 @@ const connectdb = async () => {
   const dburl = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@jobposts.pazpu.mongodb.net/?retryWrites=true&w=majority&appName=JobPosts`
   //mongodb + srv://yaswanthgummadi673:JewfFzSB3I3Bm2U2@jobposts.pazpu.mongodb.net/?retryWrites=true&w=majority&appName=JobPosts
   mongoose.connect(dburl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
     .then((result) => console.log("mongodb connected "))
     .catch((err) => console.log(err +"mongodb not connected"));
